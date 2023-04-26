9c91a2ac2cb1db52b7f598a695ce9decc8cc0bf6
Purav <puravparabdev@gmail.com>
2023-04-26 20:48:33 +0400

    django setup fixed

--- api/server/settings/base.py
+++ api/server/settings/base.py
@@ -36,16 +36,29 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 # Application definition
 
 INSTALLED_APPS = [
+	# Run Whitenoise while debug = False
+	'whitenoise.runserver_nostatic',
+
 	'django.contrib.admin',
 	'django.contrib.auth',
 	'django.contrib.contenttypes',
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
+
+	# djanog-cors-headers
+	'corsheaders',
 ]
 
 MIDDLEWARE = [
 	'django.middleware.security.SecurityMiddleware',
+
+	# django-cors-headers
+	'corsheaders.middleware.CorsMiddleware',
+
+	# whitenoise
+	"whitenoise.middleware.WhiteNoiseMiddleware",
+
 	'django.contrib.sessions.middleware.SessionMiddleware',
 	'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
@@ -104,6 +117,15 @@ AUTH_PASSWORD_VALIDATORS = [
 	},
 ]
 
+# CORS
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS").split(' ')
+
+CORS_ALLOW_METHODS = [
+	'GET',
+	'OPTIONS',
+	'POST'
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -121,9 +143,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = Path(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+	BASE_DIR / "static"
+]
+STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
