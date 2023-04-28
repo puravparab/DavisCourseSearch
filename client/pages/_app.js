939da33c30c6888ef25b4ff5768d91de7177254f
Purav <puravparabdev@gmail.com>
2023-04-28 08:34:25 +0400

    nextjs setup

---
+++ client/pages/_app.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Layout from '../components/Layout.js'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<>
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+				<meta name="author" content="" />
+				<meta name="twitter:card" content="summary" />
+				<meta name="twitter:image" content="" />
+				<meta name="twitter:creator" content="" />
+				<meta property="og:type" content="website" />
+				<meta property="og:image" content="" />
+			</Head>
+			
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</>
+	)
+}
+
+export default MyApp
